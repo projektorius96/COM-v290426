249b8cc62cb701eab5c1fbff0edff51521c6168e
LG <34185919+projektorius96@users.noreply.github.com>
2026-05-14 19:51:13 +0300

    code-rev: removing the redundant lines

--- implementation/app.js
+++ implementation/app.js
@@ -6,9 +6,6 @@ export default class Stage {
         const stage = document.createElement('div');
             container.appendChild(stage);
 
-        // // Clear previous canvas[-es] (a.k.a. layer[-s])
-        stage.innerHTML = '';
-
         Object.assign(stage, { on: this.on.bind(null, stage) })
 
         // DEV_NOTE # we must return "this" (stage) in order to chain calls in a row, e.g. init().on().etc() without a need of variable reference.
