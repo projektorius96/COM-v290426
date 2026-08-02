3ee575c239c6c2dad8cb0f334ebd6fe7ac7922ef
LG <34185919+projektorius96@users.noreply.github.com>
2026-08-02 20:50:38 +0300

    code-rev: addressing #14
    This tiny code revision is just checkpoint before checking out, you can completely ignore this commit per se.

--- implementation/entry.js
+++ implementation/entry.js
@@ -22,28 +22,34 @@ export default function ({Layer, HitDetector}) {
     })
     .on(({ stage }) => {
 
-        const gridLayer = Reflect.construct(Layer, [{ id: ID.grid, stage, gridConfig: UserConfig.grid }]);
+        const 
+            gridLayer = Reflect.construct(Layer, [{ id: ID.grid, stage, gridConfig: UserConfig.grid }])
+            ,
+            circleLayer = Reflect.construct(Layer, [{ stage, gridConfig: UserConfig.grid }])
+            ;
+
         Views.Grid.draw({
             container: gridLayer,
-            options: {}
         });
 
-        const circleLayer = Reflect.construct(Layer, [{ stage, gridConfig: UserConfig.grid }]);
-        Views.Line.draw({
-            container: circleLayer,
-            options: {
-                points: [...setRange(...Helpers.QUADRANT.Q4).map((coords)=> coords = {x: coords, y: coords} )],
-                scale: [2, 2],
-                color: COLOR.red,
-                lineWidth: 20,
-                opacity: 1,
-            },
-            // Called at the end of every render (initial + every resize repaint)
-            onAfterRender: ({container, options}) => {
-                // Do some finalization work here,.. 
-                // ..except calling {container.render()} itself - that would halt your runtime due to recursive calls!
-            }
-        });
+        gridLayer.onRender(()=>{
+
+            Views.Line.draw({
+                container: circleLayer,
+                options: {
+                    points: [...setRange(...Helpers.QUADRANT.Q4).map((coords)=> coords = {x: coords, y: coords} )],
+                    scale: [1, 1],
+                    color: COLOR.red,
+                    lineWidth: 20,
+                    opacity: 1,
+                },
+                // Called at the end of every render (initial + every resize repaint)
+                onAfterRender: ({container, options}) => {
+                    console.log(Math.random())
+                }
+            });
+            
+        })
 
     });
     
